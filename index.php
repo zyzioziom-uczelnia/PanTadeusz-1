e8e6e05ee4b7001f443bc2036b59401d43cf8e7c
Patryk Kalinowski <patryk.kalinowski@gmail.com>
2015-11-14 12:06:11 +0100

    Update

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pan Tadeusz, czyli ostatni zajazd na Litwie</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+</head>
+<body class="container">
+
+		<h1 class="text-center">Pan Tadeusz, czyli ostatni zajazd na Litwie</h1>
+		<h2 class="text-center">Historia szlachecka z roku 1811 i 1812 we dwunastu ksiegach wierszem</h2>
+		<div class"row">
+		  <div class="col-md-3 panel panel-default">
+		    <div class="panel-body"
+			<p>Spis Tresci</p>
+			<ul>
+			<?php
+			  
+				<li><a href="./">Strona Glowna</a></li>
+				<li><a href="./k1.html">Księga I</a></li>
+				<li><a href="./k2.html">Księga II</a></li>
+				<li><a href="./k3.html">Księga III</a></li>
+				<li><a href="./k4.html">Księga IV</a></li>
+				<li><a href="./k5.html">Księga V</a></li>
+				<li><a href="./k6.html">Księga VI</a></li>
+				<li><a href="./k7.html">Księga VII</a></li>
+				<li><a href="./k8.html">Księga VIII</a></li>
+				<li><a href="./k9.html">Księga IX</a></li>
+				<li><a href="./k10.html">Księga X</a></li>
+				<li><a href="./k11.html">Księga XI</a></li>
+				<li><a href="./k12.html">Księga XII</a></li>
+			</ul>
+		  </div>
+			</div>
+		  <div class="col-md-9 panel">
+			Ksiegi
+		  </div>
+		</div>
+	</body>	
+</html>
