728f84187f886e0149e65a5ccd5b509e3b36b86d
Patryk Kalinowski <patryk.kalinowski@netguru.pl>
2015-12-10 20:35:47 +0100

    Database SELECT

---
+++ database.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'config.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql = "SELECT id, title, reflection FROM reflections";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Numer: " . $row["id"]. " - Tytuł: " . $row["title"]. "<br /> " . $row["reflection"]. "<hr />";
+    }
+} else {
+    echo "";
+}
+$conn->close();
+?>
